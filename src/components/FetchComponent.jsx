550f2af4069a4f2b19574950f01035aae5652d27
ErnestoCarballo <ernestowcarballo12@gmail.com>
2022-05-15 22:33:44 -0700

    second commit

--- src/components/FetchComponent.jsx
+++ src/components/FetchComponent.jsx
@@ -23,10 +23,26 @@ const FetchComponent = () => {
         setPokemon(jsonResponse.results)
     }
 
+    const fetchPokemonAxios = () =>{
+        axios.get("https://pokeapi.co/api/v2/pokemon?limit=807")
+        .then(response=>{
+            console.log(response.data.results)
+            setPokemon(response.data.results)
+        })
+        .catch(err=>console.log(err))
+    }
+
+    const fetchPokemonAxiosAwait = async() =>{
+        const response = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=807")
+        setPokemon(response.data.results)
+    }
+
     return(
         <div>
             <button onClick={fetchPokemon}>Who are those Pokemon!?</button>
             <button onClick={fetchPokemonAwait}>Who are those Pokemon!?</button>
+            <button onClick={fetchPokemonAxios}>Who are those Pokemon!?</button>
+            <button onClick={fetchPokemonAxiosAwait}>Who are those Pokemon!?</button>
         {pokemon.map((mon, i)=>{
             return (<p key={i}>{mon.name}</p>)
         })}
